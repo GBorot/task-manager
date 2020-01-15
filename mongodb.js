1caba2b5b107c6caf49fd0d603bbcae910d01ac7
Gautier Borot <g.borot@gmail.com>
2020-01-15 14:49:49 +0100

    created two collections and inserted one or many documents inside

--- mongodb.js
+++ mongodb.js
@@ -17,9 +17,63 @@ MongoClient.connect(
     // console.log("Connected correctly");
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-      name: "Gautier",
-      age: 28
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Gautier",
+    //     age: 28
+    //   },
+    //   (error, result) => {
+    //     // http://mongodb.github.io/node-mongodb-native/3.4/api/Collection.html#~insertOneWriteOpCallback
+    //     if (error) {
+    //       return console.log("Unable to insert user");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Gautier",
+    //       age: 28
+    //     },
+    //     {
+    //       name: "Gunter",
+    //       age: 27
+    //     }
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       console.log("Unable to insert documents!");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Learn Node JS",
+          completed: false
+        },
+        {
+          description: "Empty trash kitchen",
+          completed: true
+        },
+        {
+          description: "Kiss Gratiane today",
+          completed: true
+        }
+      ],
+      (error, result) => {
+        if (error) {
+          console.log("Unable to insert task");
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
