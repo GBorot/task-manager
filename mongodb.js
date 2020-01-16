202d5af534afa75c0a47584fe27a56d69ec7b288
Gautier Borot <g.borot@gmail.com>
2020-01-16 09:59:50 +0100

    exercise on tasks: research by id and truthy completed array

--- mongodb.js
+++ mongodb.js
@@ -31,16 +31,29 @@ MongoClient.connect(
     //   }
     // );
 
-    db.collection("users")
-      .find({ age: 28 })
-      .toArray((error, users) => {
-        console.log(users);
-      });
+    // db.collection("users")
+    //   .find({ age: 28 })
+    //   .toArray((error, users) => {
+    //     console.log(users);
+    //   });
 
-    db.collection("users")
-      .find({ age: 28 })
-      .count((error, users) => {
-        console.log(users);
+    // db.collection("users")
+    //   .find({ age: 28 })
+    //   .count((error, users) => {
+    //     console.log(users);
+    //   });
+
+    db.collection("tasks").findOne(
+      { _id: new ObjectID("5e1f183fee0e66bdaa7d6fb3") },
+      (error, task) => {
+        console.log(task);
+      }
+    );
+
+    db.collection("tasks")
+      .find({ completed: true })
+      .toArray((error, tasks) => {
+        console.log(tasks);
       });
   }
 );
