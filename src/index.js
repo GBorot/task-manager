9dd0831d31b467e3b690b2ed4d03ab7f449fc812
Gautier Borot <g.borot@gmail.com>
2020-01-22 18:45:45 +0100

    created an update user with PATCH http request. We also created a function to check if the updates are valid

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ app.use(express.json());
 
 // all HTTP status : https://httpstatuses.com/
 
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
@@ -18,15 +27,6 @@ app.post("/users", async (req, res) => {
     res.status(201).send(user);
   } catch (e) {
     res.status(400).send(e);
-  }
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
   }
 });
 
@@ -45,14 +45,30 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
-app.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every(update => {
+    return allowedUpdates.includes(update);
+  });
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
 
   try {
-    await task.save();
-    res.status(201).send(task);
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!user) {
+      res.status(404).send();
+    }
+
+    res.send(user);
   } catch (e) {
-    res.status(400).send();
+    res.status(400).send(e);
   }
 });
 
@@ -62,6 +78,17 @@ app.get("/tasks", async (req, res) => {
     res.send(tasks);
   } catch (e) {
     res.status(500).send();
+  }
+});
+
+app.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send();
   }
 });
 
