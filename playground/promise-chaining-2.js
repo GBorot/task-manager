d82e4b108bbd6e615bbb6e48737b1cda7b80d0fb
Gautier Borot <g.borot@gmail.com>
2020-01-22 15:21:22 +0100

    used await in a context.. i understand th utiliy now

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,13 +1,28 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("5e26ff9aef8b7174fe96c738")
-  .then(task => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then(result => {
-    console.log(result);
+// Task.findByIdAndDelete("5e26ff9aef8b7174fe96c738")
+//   .then(task => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+
+const findAndDelete = async (id, status) => {
+  const task = await Task.findByIdAndDelete(id);
+  const pendingTasks = await Task.countDocuments({ status });
+
+  return pendingTasks;
+};
+
+findAndDelete("5e26fefcef8b7174fe96c735", false)
+  .then(count => {
+    console.log(count);
   })
   .catch(e => {
     console.log(e);
