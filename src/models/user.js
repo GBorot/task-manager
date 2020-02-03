f3139d591f6291f9beed6195afbb5b63007543cf
Gautier Borot <g.borot@gmail.com>
2020-02-03 16:43:34 +0100

    created a validation credentials on email and password

--- src/models/user.js
+++ src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -41,6 +42,26 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+// ---------------------------------------------------------------
+// Credentials
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login!");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login!");
+  }
+
+  return user;
+};
+
+// ---------------------------------------------------------------
+// Hash the plain text password before saving
 // arrow function don't bind THIS instead of standard functions do
 userSchema.pre("save", async function(next) {
   // this gives access to the user before being stored
