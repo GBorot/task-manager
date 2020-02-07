fe7ad15c9394a5e621e61acf0e8dbc25c5978ef5
Gautier Borot <g.borot@gmail.com>
2020-02-07 16:08:57 +0100

    so we use toJSON to manipulate what we want to return as data, and then express stringifies the JSON data we return

--- src/index.js
+++ src/index.js
@@ -28,19 +28,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-const jwt = require("jsonwebtoken");
-
-const myFunction = async () => {
-  const token = jwt.sign({ _id: "abc123" }, "login", {
-    expiresIn: "7 days"
-  });
-
-  console.log(token);
-
-  const data = jwt.verify(token, "login");
-
-  console.log(data);
-};
-
-myFunction();
