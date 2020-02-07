c76d3356cc5b03b8a5f24fc572f69862d9f01c5a
Gautier Borot <g.borot@gmail.com>
2020-02-07 17:08:10 +0100

    now we can delete and update tasks logged in

--- src/routers/task.js
+++ src/routers/task.js
@@ -46,7 +46,7 @@ router.get("/tasks/:id", auth, async (req, res) => {
   }
 });
 
-router.patch("/tasks/:id", async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["description", "completed"];
   const isValidOperation = updates.every(update => {
@@ -58,22 +58,19 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findById(req.params.id);
-
-    updates.forEach(update => {
-      task[update] = req.body[update];
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id
     });
-
-    await task.save();
-
-    // const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-    //   new: true,
-    //   runValidators: true
-    // });
 
     if (!task) {
       res.status(404).send();
     }
+
+    updates.forEach(update => {
+      task[update] = req.body[update];
+    });
+    await task.save();
 
     res.send(task);
   } catch (e) {
@@ -81,9 +78,12 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 });
 
-router.delete("/tasks/:id", async (req, res) => {
+router.delete("/tasks/:id", auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id);
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id
+    });
 
     if (!task) {
       res.status(404).send();
