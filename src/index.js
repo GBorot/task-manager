d8862f010b3511e8305f22bf1f18f4afbcc64d1b
Gautier Borot <g.borot@gmail.com>
2020-02-07 17:13:11 +0100

    delete some comments

--- src/index.js
+++ src/index.js
@@ -6,19 +6,6 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//   // console.log(req.method, req.path);
-//   if (req.method === "GET") {
-//     res.send("GET requests are disabled");
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   res.status(503).send("Please try again later, the app is in maintenance...");
-// });
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
