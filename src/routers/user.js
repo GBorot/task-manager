564f50945a481f82acff2367045c70a119174483
Gautier Borot <g.borot@gmail.com>
2020-02-07 15:09:13 +0100

    created a logout all route to empty all tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -43,6 +43,18 @@ router.post("/users/logout", auth, async (req, res) => {
   }
 });
 
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+
+    await req.user.save();
+
+    req.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
