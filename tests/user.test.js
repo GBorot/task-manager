038eaba57c22f5f05dd37993f0e1828edf41eb70
Gautier Borot <g.borot@gmail.com>
2020-02-18 17:12:11 +0100

    tests login user and non user

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,16 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user");
 
-beforeEach(() => {
-  console.log("beforeEach");
-});
+const userOne = {
+  name: "Mike",
+  email: "mike@exemple.com",
+  password: "56what!!"
+};
 
-afterEach(() => {
-  console.log("afterEach");
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
 });
 
 test("Should signup a new user", async () => {
@@ -18,4 +22,24 @@ test("Should signup a new user", async () => {
       password: "MyPass777!"
     })
     .expect(201);
+});
+
+test("Should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password
+    })
+    .expect(200);
+});
+
+test("Should not login nonexistent user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: "Thisisnotmypass"
+    })
+    .expect(400);
 });
