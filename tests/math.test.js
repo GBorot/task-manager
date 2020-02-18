ed71ba3c601a758e100e3162493c45dc1a849e04
Gautier Borot <g.borot@gmail.com>
2020-02-18 16:24:19 +0100

    created 2 tests with setTimeOut function and async/await testing and done

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,8 @@
 const {
   calculateTip,
   fahrenheitToCelsius,
-  celsiusToFahrenheit
+  celsiusToFahrenheit,
+  add
 } = require("../src/math");
 
 test("Should calculate total with tip", () => {
@@ -24,4 +25,23 @@ test("Should convert 32 F to 0 C", () => {
 test("Should convert 0 C to 32 F", () => {
   const tempInFahrenheit = celsiusToFahrenheit(0);
   expect(tempInFahrenheit).toBe(32);
+});
+
+// test("Async test demo", done => {
+//   setTimeout(() => {
+//     expect(1).toBe(2);
+//     done();
+//   }, 2000);
+// });
+
+test("Should add 2 numbers", done => {
+  add(2, 3).then(sum => {
+    expect(sum).toBe(5);
+    done();
+  });
+});
+
+test("Should add two numbers async/await", async () => {
+  const sum = await add(10, 22);
+  expect(sum).toBe(32);
 });
