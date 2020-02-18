1b91f7f7d5dccaa97affb4f81e6297744c64380c
Gautier Borot <g.borot@gmail.com>
2020-02-18 10:43:27 +0100

    my API key has been refused so i will copy the code to go further

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,25 +1,12 @@
-// const sgMail = require("@sendgrid/mail");
-// const sendgridAPIKey =
-//   "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";
-
-// sgMail.setApiKey(sendgridAPIKey);
-
-// sgMail.send({
-//   to: "g.borot@gmail.com",
-//   from: "g.borot@gmail.com",
-//   subject: "This is my first email!",
-//   text: "I hope this email gets to you"
-// });
-
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(
-  "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"
-);
-const msg = {
+const sendgridAPIKey =
+  "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";
+
+sgMail.setApiKey(sendgridAPIKey);
+
+sgMail.send({
   to: "g.borot@gmail.com",
   from: "g.borot@gmail.com",
-  subject: "Sending with Twilio SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<strong>and easy to do anywhere, even with Node.js</strong>"
-};
-sgMail.send(msg);
+  subject: "This is my first email!",
+  text: "I hope this email gets to you"
+});
