d1e2dcc59c5941de58625eb225e82526095d8acd
Gautier Borot <g.borot@gmail.com>
2020-02-18 16:02:55 +0100

    created a default value to tip and a new test for it

--- tests/math.test.js
+++ tests/math.test.js
@@ -4,8 +4,10 @@ test("Should calculate total with tip", () => {
   const total = calculateTip(10, 0.3);
 
   expect(total).toBe(13);
+});
 
-  //   if (total !== 13) {
-  //     throw new Error("Total tip should be 13. Got " + total);
-  //   }
+test("Should calculate total with default tip", () => {
+  const total = calculateTip(10);
+
+  expect(total).toBe(12.5);
 });
