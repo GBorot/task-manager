0bb27fd65992e1fecec26e02ba379b9981674a6c
Gautier Borot <g.borot@gmail.com>
2020-02-18 16:53:39 +0100

    created our first test on API endpoint for signup user. But we need now to delete this document for re running the tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,1 +1,13 @@
 const request = require("supertest");
+const app = require("../src/app");
+
+test("Should signup a new user", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      name: "Gautier",
+      email: "g.qsdfh@gmail.com",
+      password: "MyPass777!"
+    })
+    .expect(201);
+});
